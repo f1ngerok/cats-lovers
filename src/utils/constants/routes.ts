c2e20ac211c925a53f244812dc736b3e7fe09be5
Oleg Makarov <f1ngerokk@gmail.com>
2025-04-24 00:21:44 +0200

    Added missed

--- src/utils/constants/routes.ts
+++ src/utils/constants/routes.ts
@@ -1,7 +1,8 @@
 export const ROUTES = {
   HOME: '/',
   ABOUT: '/about',
-  ARTICLES: '/articles',
   ARTICLE_CREATE: '/articles/create',
+  ARTICLES_DETAIL: '/article/:articleId',
+  ARTICLES_EDIT: '/article/:articleId/edit',
   SIGN_IN: '/auth/sign-in',
 } as const;
