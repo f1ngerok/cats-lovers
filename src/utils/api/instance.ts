009e0d8ba0572749059115f1152ad42011c86191
Oleg Makarov <f1ngerokk@gmail.com>
2025-04-24 00:01:27 +0200

    Added setting X-API-KEY header to axios instance

--- src/utils/api/instance.ts
+++ src/utils/api/instance.ts
@@ -1,6 +1,9 @@
-import axios from 'axios';
+import axios, { AxiosHeaders } from 'axios';
 
 export const api = axios.create({
   baseURL: process.env.API_HOST,
   timeout: 10_000,
+  headers: new AxiosHeaders({
+    'X-API-KEY': process.env.API_SECRET_KEY,
+  }),
 });
