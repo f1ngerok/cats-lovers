50f2fd3bb8bae3be4d5765320c5dc474a8fa8f77
Oleg Makarov <f1ngerokk@gmail.com>
2025-04-27 00:50:59 +0200

    Added logic to check if user is authenticated in protected layout

--- src/routes/layouts/protected.tsx
+++ src/routes/layouts/protected.tsx
@@ -1,7 +1,22 @@
 import type { FC } from 'react';
 
-import { Outlet } from 'react-router';
+import Cookies from 'js-cookie';
+import { useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router';
+
+import { STORE } from '@/utils/constants';
+import { ROUTES } from '@/utils/constants/routes';
 
 export const ProtectedLayout: FC = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (!Cookies.get(STORE.COOKIES.AUTH_TOKEN_KEY)) {
+      localStorage.setItem(STORE.LOCAL_STORAGE.REQUESTED_ROUTE_KEY, pathname);
+      navigate(ROUTES.SIGN_IN);
+    }
+  }, []);
+
   return <Outlet />;
 };
