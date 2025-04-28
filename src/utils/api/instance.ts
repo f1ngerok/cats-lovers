701e84a8b6daa05deab463dca087f89dd2c4ba4f
Oleg Makarov <f1ngerokk@gmail.com>
2025-04-28 08:47:12 +0200

    Added passing auth token to requests header if token exist

--- src/utils/api/instance.ts
+++ src/utils/api/instance.ts
@@ -1,9 +1,18 @@
 import axios, { AxiosHeaders } from 'axios';
 
+import { STORE } from '@/utils/constants';
+
+const headers = new AxiosHeaders({
+  'X-API-KEY': process.env.API_SECRET_KEY,
+});
+
+const authToken = localStorage.getItem(STORE.LOCAL_STORAGE.REQUESTED_ROUTE_KEY);
+if (authToken) {
+  headers.set('Authorization', `Bearer ${authToken}`);
+}
+
 export const api = axios.create({
   baseURL: process.env.API_HOST,
   timeout: 10_000,
-  headers: new AxiosHeaders({
-    'X-API-KEY': process.env.API_SECRET_KEY,
-  }),
+  headers,
 });
